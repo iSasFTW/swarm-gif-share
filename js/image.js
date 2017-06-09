2e9b99968b35624b4839d4806fc944fb8f25ec66
iSasFTW <saska.kolehmainen@hotmail.com>
2017-06-09 23:57:29 +0300

    Added tipping functionality

--- js/image.js
+++ js/image.js
@@ -1,5 +1,9 @@
+$(document).ready(function() {
+    $("#tipbox").hide();
+});
 var swarm = Swarm.at("http://swarm-gateways.net");
 var imageData = "";
+var uploader = "";
 (function($) {
     $.QueryString = (function(a) {
         if (a == "") return {};
@@ -14,12 +18,25 @@ var imageData = "";
     })(window.location.search.substr(1).split('&'))
 })(jQuery);
 
-
 swarm.download($.QueryString["i"]).then(function(buffer) {
     imageData = JSON.parse(buffer.toString());
     var image = document.getElementById("image");
     var caption = document.getElementById("caption");
     image.src = imageData.image;
     caption.innerHTML = imageData.caption;
+    uploader = imageData.poster;
+    
 });
 
+function openTip() {
+    document.getElementById("tipbutton").style.display = "none";
+    $("#tipbox").show();
+}
+
+function tip() {
+    var szaboAmount = $("#tip-amount").value;
+    var weiAmount = web3.toWei(szaboAmount, "szabo");
+    var sender = web3.eth.accounts[0];
+    var recipient = uploader;
+    web3.eth.sendTransaction({from: sender, to: recipient, value: weiAmount});
+}
